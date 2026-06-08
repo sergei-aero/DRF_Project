e23919b001f3f9e56c0db9ecb5b4f84cbe488208
sergei-aero <pavlov.aero@gmail.com>
2026-06-08 18:12:19 +0300

    HW_30.2: Сериализация/фильтрация

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('materials.urls')),
+    path('api/', include('users.urls')),
 ]
 
 if settings.DEBUG:
